9ea7d7f9c41ae4bf128a8c82adb7ba759486bdc0
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-01-21 10:25:32 +0530

    display-likes completed

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -17,17 +17,5 @@
 let arr1 = [1, 2, 3, 4, 5, 67, 2];
 let arr2 = [2, 3, 45, 1, 3, 67];
 
-let unique = new Set();
-
-for (let i = 0; i < arr1.length; i++) unique.add(arr1[i]);
-console.log(unique);
-let count = 0;
-for (let i = 0; i < arr2.length; i++) {
-  if (unique.has(arr2[i])) {
-    console.log("arr2[i]", arr2[i]);
-    count++;
-    unique.delete(arr2[i]);
-  }
-}
 console.log(unique);
 console.log(count);
--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,27 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  let len = arr.length;
+  switch (len) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${arr[0]} likes this`;
+    case 2:
+      return `${arr[0]} and ${arr[1]} like this`;
+    case 3:
+      return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+    default:
+      return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+  }
+}
 
 module.exports = displayLikes;
+
+console.log(displayLikes(["Peter"]));
+
+// let len = arr.length;
+
+// if (len == 0) return "no one likes this";
+// else if (len == 1) return `${arr[0]} likes this`;
+// else if (len == 2) return `${arr[0]} and ${arr[1]} like this`;
+// else if (len == 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+// else return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
