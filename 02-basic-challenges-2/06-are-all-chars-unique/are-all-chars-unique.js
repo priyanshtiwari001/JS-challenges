b67d6de825601dfabe86aded637aeba989353700
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-01-24 13:15:37 +0530

    chars-unquie completed

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,26 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//   let arr = str.split("");
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (arr[i] == arr[i + 1]) {
+//       count++;
+//     }
+//   }
 
-module.exports = areAllCharactersUnique;
+//   return count > 0 ? false : true;
+// }
+
+function areAllCharactersUnique(str) {
+  let charSet = new Set();
+  console.log(charSet);
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    if (charSet.has(element)) {
+      return false;
+    }
+    charSet.add(element);
+  }
+  return true;
+}
+//module.exports = areAllCharactersUnique;
+console.log(areAllCharactersUnique("abcdd"));
