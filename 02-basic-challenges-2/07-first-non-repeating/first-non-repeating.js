021cb2bbbef5e9535220d9b4aae8147fece0bca9
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-01-26 22:58:00 +0530

    not done yet non-repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,13 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+ 
+    for(let i=0;i<str.length;i++){
+        if(str[i]!=str[i+1]){
+            return str[i];
+        }
+    }
+    
 
-module.exports = findFirstNonRepeatingCharacter;
+ 
+
+//module.exports = findFirstNonRepeatingCharacter;
+findFirstNonRepeatingCharacter("aabccdd");
